e685cc9ebc242a4548a00d6e518a35628f913f0b
t.bertram-koehler <t.bertramkoehler@gmail.com>
2018-03-15 13:34:36 +0100

    add unhappy test for StaticEntityClassResolver

---
+++ src/test/java/de/toberkoe/pluto/extensions/integration/persistence/config/discovery/StaticEntityClassResolverTest.java
@@ -0,0 +1,14 @@
+package de.toberkoe.pluto.extensions.integration.persistence.config.discovery;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class StaticEntityClassResolverTest {
+
+    @Test
+    void resolve() {
+        StaticEntityClassResolver resolver = new StaticEntityClassResolver(true);
+        assertThrows(EntityDiscoveryException.class, () -> resolver.resolve(String.class));
+    }
+}
